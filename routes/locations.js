e73bd4776a305f72426382cc456055e3036ed358
thoglass <t.glasauer@web.de>
2019-08-07 14:51:28 +0200

    get detailed location including bee line distance

--- routes/locations.js
+++ routes/locations.js
@@ -68,10 +68,54 @@ router.post('/', upload.single('file'), (req, res, next) => {
 
     })
 });
+// get location names
 router.get('/', (req, res, next) => {
-  // select all locaton names from db
+  // select all locaton names from db, returns array
   res.send(db().select("name"));
   res.end();
 })
+
+// selects the first location that matches a given name
+const getLocationByName = (name, callback) => {
+  const location = db().first({name: name});
+  if(!location) return callback(new Error('no location matching' + name))
+
+  return callback(null, location);
+};
+
+// calculates bee distance to piloteers office
+// input: location object, e.g. {name: "PiloteersBerlin", lat: 52.502931, lng: 13.408249, info: ""};
+const beeLineToPiloteersBerlin = (location, callback) => {
+  if(!location.lat || !location.lng) return callback(new Error("Make sure your location contains lat and lng properties"));
+  const piloteersBerlin = {name: "PiloteersBerlin", lat: 52.502, lng: 13.408249, info: ""};
+  // calculate bee line distance on a sphere
+  let distance = 6378.388 * Math.acos( Math.sin(piloteersBerlin.lat) * Math.sin(location.lat) + Math.cos(piloteersBerlin.lat) * Math.cos(location.lat) * Math.cos(piloteersBerlin.lng - location.lng));
+
+  distance = (distance > 0) ?  distance : -distance;
+
+  return callback(null, distance)
+};
+// get location-details by name
+router.get('/:name', (req, res, next) => {
+
+  const locationname = req.param.name;
+
+  getLocationByName(locationname, (err, location) => {
+
+    if(err) return next(err);
+
+    beeLineToPiloteersBerlin(location, (err, distance) => {
+      if(err) return next(err);
+
+      location.distance = distance;
+
+      res.send(location);
+      res.end();
+  })
+
+  })
+
+
+});
 
 module.exports = router;
